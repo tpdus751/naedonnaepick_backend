4bd883b4be4938f9ea1c7d86f44618d8268389a5
ljsuuuuuuuu <cakalu56@gmail.com>
2025-06-05 13:03:42 +0900

    aa

--- src/main/java/com/naedonnaepick/backend/restaurant/controller/RestaurantAPIController.java
+++ src/main/java/com/naedonnaepick/backend/restaurant/controller/RestaurantAPIController.java
@@ -5,6 +5,8 @@ import com.naedonnaepick.backend.restaurant.entity.RestaurantMenu;
 import com.naedonnaepick.backend.restaurant.service.RestaurantMenuService;
 import com.naedonnaepick.backend.restaurant.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,16 +21,26 @@ public class RestaurantAPIController {
     @Autowired
     private RestaurantMenuService restaurantMenuService;
 
-
     @GetMapping("/recommended")
     public List<RestaurantEntity> sendRecommendedRestaurants(
             @RequestParam(name = "location") String location,
-            @RequestParam(name = "minPrice", required = false, defaultValue = "0") int minPrice,
-            @RequestParam(name = "maxPrice", required = false, defaultValue = "10000") int maxPrice
+            @RequestParam(name = "minPrice") int minPrice,
+            @RequestParam(name = "maxPrice") int maxPrice
     ) {
         // Service 호출
         return restaurantService.getRecommendedRestaurants(location, minPrice, maxPrice);
     }
+
+    @GetMapping("/search")
+    public List<RestaurantEntity> sendSearchRestaurants(
+            @RequestParam(name = "searchText") String searchText,
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        return restaurantService.getRestaurantsBySearchText(searchText, pageable);
+    }
+
 
     @GetMapping("/menus")
     public List<RestaurantMenu> sendRestaurantMenus(
